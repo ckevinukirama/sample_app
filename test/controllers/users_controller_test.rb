dfc41d6613c18d5dd523a53881fdbfbd66d285f6
ckevinukirama <christopher.herijanto@ukirama.com>
2024-02-16 10:14:11 +0700

    Finish user Signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get new" do
-    get signup_path
-assert_response :success
+  test "should get show" do
+    get users_show_url
+    assert_response :success
   end
 end
