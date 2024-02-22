fba261d79615fbd2db6d062c2f3bff7b45ff0910
ckevinukirama <christopher.herijanto@ukirama.com>
2024-02-22 11:16:50 +0700

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
-
-
 class ApplicationController < ActionController::Base
-
   include SessionsHelper
-  end
-  
+
+  private
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+end
