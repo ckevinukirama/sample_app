e762762fbdcbfb5eb8b4d929cdf5b2e508b1e264
ckevinukirama <christopher.herijanto@ukirama.com>
2024-02-23 16:35:47 +0700

    fix margin top for user_info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
+        redirect_to login_path, status: :see_other
       end
     end
 end
